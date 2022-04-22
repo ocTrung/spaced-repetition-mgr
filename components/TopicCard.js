9085a8b201162a7357975348ea3f2149fa7b4053
octrung <t.nguyenvo@eagles.oc.edu>
2022-04-21 22:44:13 -0500

    prominent display for next review session date

--- components/TopicCard.js
+++ components/TopicCard.js
@@ -20,44 +20,58 @@ export default function TopicCard({ topic, index }) {
     setDelay(index * 200 + 'ms')
   }, [])
 
+  const nextDate = getNextReviewDate(topic)
+  const day = nextDate.getDate()
+  const month = nextDate.toLocaleString('default', { month: 'long' })
+
   return (
-    <div
-      key={topic.title}
-      className={styles.card}
-      style={{ animationDelay: delay }}
-    >
-      <section className={styles.topSection}>
-        <header className={styles.cardHeader}>
-          <a className={styles.cardTitle}>
-            {topic.title}
-          </a>
-        </header>
-        <button
-          className={showUpdater ? styles.gradeToggleOpen : styles.gradeToggleClosed}
-          onClick={() => setShowUpdater(!showUpdater)}
-        >
-          update
-        </button>
-        {
-          showUpdater &&
-          <>
-            <SessionGrader topic={topic} />
-            <button
-              onClick={() => mutation.mutate(topic)}
-            >
-              stop tracking
-            </button>
-          </>
-        }
-      </section>
+    <div className={styles.card}>
+      <div className={styles.cardLeft}>
+        <div className={styles.date}>
+          <p className={styles.dateTitle}>due</p>
+          <p className={styles.day}>{day}</p>
+          <p className={styles.month}>{month}</p>
+        </div>
+      </div>
+      <div
+        key={topic.title}
+        className={styles.main}
+        style={{ animationDelay: delay }}
+      >
+        <section className={styles.topSection}>
+          <header className={styles.cardHeader}>
+            <a className={styles.cardTitle}>
+              {topic.title}
+            </a>
+          </header>
+          <button
+            className={showUpdater ? styles.gradeToggleOpen : styles.gradeToggleClosed}
+            onClick={() => setShowUpdater(!showUpdater)}
+          >
+            update
+          </button>
+          {
+            showUpdater &&
+            <>
+              <SessionGrader topic={topic} />
+              <button
+                onClick={() => mutation.mutate(topic)}
+              >
+                stop tracking
+              </button>
+            </>
+          }
+        </section>
 
-      <hr className={styles.hr}></hr>
+        <hr className={styles.hr}></hr>
 
-      <ul className={styles.info}>
-        <li className={styles.next}>next: {getNextReviewDate(topic).toDateString()}</li>
-        <li className={styles.infoItem}>last: {new Date(topic.lastReviewed).toDateString()}</li>
-      </ul>
-      <GradeVisualizer sessionGrades={topic.sessionGrades} />
+        <ul className={styles.info}>
+          {/* <li className={styles.next}>next: {getNextReviewDate(topic).toDateString()}</li> */}
+          <li className={styles.infoItem}>current interval: {topic.intervals[topic.intervals.length - 1]}</li>
+          <li className={styles.infoItem}>started: {new Date(topic.startDate).toDateString()}</li>
+        </ul>
+        <GradeVisualizer sessionGrades={topic.sessionGrades} />
+      </div>
     </div>
   )
 }
