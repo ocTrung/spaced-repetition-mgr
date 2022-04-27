29e83d6287698b69c5f24fade7fb2589eeadb720
octrung <t.nguyenvo@eagles.oc.edu>
2022-04-27 17:44:48 -0500

    added logo to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@ import styles from '@/styles/Navbar.module.scss'
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
+      <h1 className={styles.h1}>SPR</h1>
       <button className={styles.signOut} onClick={signOut}>sign out</button>
     </nav>
   )
